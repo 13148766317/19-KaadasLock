83b561b84c2f8d91d12efc8184d02c7db26d41a4
Fighting <5337425+uiuihafi@user.noreply.gitee.com>
2021-08-24 15:52:02 +0800

    更新

---
+++ KaadasLock/KDSClasses/KDSLibs/XMStream/XMStreamComCtrl.framework/Headers/PPCS_Type.h
@@ -0,0 +1,36 @@
+#if !defined(__INCLUDED_TYPE_DEFINE____H)
+#define __INCLUDED_TYPE_DEFINE____H
+
+#if defined WIN32DLL || defined LINUX
+typedef int INT32;
+typedef unsigned int UINT32;
+#endif
+
+typedef short INT16;
+typedef unsigned short UINT16;
+
+typedef char CHAR;
+typedef signed char	 SCHAR;
+typedef unsigned char UCHAR;
+
+typedef long LONG;
+#if TARGET_OS_IPHONE
+typedef unsigned long ULONG;
+#elif TARGET_OS_MAC
+typedef unsigned long ULONG2;
+#endif
+
+#ifndef bool
+#define bool	CHAR
+#endif
+
+#ifndef _ARC_COMPILER
+#ifndef true
+#define true	1
+#endif
+
+#ifndef false
+#define false	0
+#endif
+#endif ////#ifndef _ARC_COMPILER
+#endif  //__INCLUDED_TYPE_DEFINE_H
